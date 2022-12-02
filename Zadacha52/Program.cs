3ed5f4ef147cc5f31d5d8fcbdbb0685191832b58
Irina <ip_scheglova@mail.ru>
2022-12-03 00:25:54 +0500

    Home Work Number 52

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+double[] average = new double[columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+for(int i = 0; i < numbers.GetLength(1); i++)
+    {
+        double sum = 0;
+        for(int j = 0; j < numbers.GetLength(0); j++)
+        {
+            sum+=Convert.ToDouble(numbers[j, i]);            
+        }
+        average[i] =Math.Round(sum / numbers.GetLength(0), 2);
+    }
+
+WriteArray(average);
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "  ");
+    }
+    Console.WriteLine();
+}
